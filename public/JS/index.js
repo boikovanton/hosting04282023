83d1421ff8c16467947472c85052a39d7e12f134
boikovanton <boikovanton1987@gmail.com>
2023-04-27 22:40:46 -0700

    Added files from website

---
+++ public/JS/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEOfQOmoz7OD9XhZz776NawuAh38-G9f4",
+  authDomain: "sdexpertrepair-e748e.firebaseapp.com",
+  projectId: "wwwsdexpertrepair",
+  storageBucket: "wwwsdexpertrepair.appspot.com",
+  messagingSenderId: "100244224994",
+  appId: "1:100244224994:web:43de17d7778793d33fd7bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
